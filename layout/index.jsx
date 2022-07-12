e2f3ff84480cf5fb45e9574ebe486c85f1c2ac6d
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2022-07-12 13:27:43 -0400

    update configuration

--- layout/index.jsx
+++ layout/index.jsx
@@ -7,6 +7,8 @@ export default function Layout({ children }) {
     <>
       <Head>
         <title>My Title</title>
+        <meta name="description" content="The purpose of a meta description is to reflect the essence of a page, but with more details and context."/>
+        <meta name="keywords" content="These, Are, Keywords"/>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Header />
